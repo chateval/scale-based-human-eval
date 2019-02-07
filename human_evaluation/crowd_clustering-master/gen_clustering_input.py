abf17b4a96b5f8a93ff7f19744e29cb84f39e852
Reno Kriz <rekriz@seas.upenn.edu>
2019-02-07 16:20:23 -0500

    Fix

--- human_evaluation/crowd_clustering-master/gen_clustering_input.py
+++ human_evaluation/crowd_clustering-master/gen_clustering_input.py
@@ -3,38 +3,45 @@ import os
 from os import listdir
 from os.path import isfile, join
 import json
+import random
 
 ## Gets name of files in a list from a directory
 def get_all_files(directory):
     files = [f for f in listdir(directory) if isfile(join(directory, f))]
     return files
 
+## Load all json files!
 def load_directory(directory):
     files = get_all_files(directory)[:1]
-    print(files)
     filepaths = [directory + "/" + file for file in files]
 
     inputs, preds, scores = [], [], []
     for path in filepaths:
+        inps, prds, scrs = [], [], []
         with open(path) as f:
             outputs = json.load(f)
 
             for result_dict in outputs["results"]:
-                inputs.append(' '.join(result_dict["input"]))
-                preds.append([" ".join(p) for p in result_dict["pred"]])
-                scores.append(result_dict["scores"])
+                inps.append(' '.join(result_dict["input"]))
+                prds.append([" ".join(p) for p in result_dict["pred"]])
+                scrs.append(result_dict["scores"])
+        inputs.append(inps)
+        preds.append(prds)
+        scores.append(scrs)
 
     print(len(inputs))
-    print(inputs[0])
-    print(preds[0])
-    print(scores[0])
+    print(inputs[0][0])
+    print(preds[0][0])
+    print(scores[0][0])
 
     return inputs, preds, scores
 
-                
+def output_format(inputs, preds, scores, output_base):
+    
                 
 
 def main(system_outputs_folder):
+    random.seed(37)
     inputs, preds, scores = load_directory(system_outputs_folder)
     
     
