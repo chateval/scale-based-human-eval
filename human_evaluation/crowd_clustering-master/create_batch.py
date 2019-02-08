94bfd4241901c51e57677de8f19df6b4a27c23a7
Reno Kriz <rekriz@seas.upenn.edu>
2019-02-08 13:33:22 -0500

    Debug

--- human_evaluation/crowd_clustering-master/create_batch.py
+++ human_evaluation/crowd_clustering-master/create_batch.py
@@ -164,6 +164,7 @@ if __name__ == "__main__":
                 print('posting HIT %d of %d'%(i, len(hits)))
                 params = [layoutparam.LayoutParameter(k,v) for k,v in h.items()]
                 print(settings)
+                print(params)
                 result = conn.create_hit(hit_layout=settings['HIT_LAYOUT_ID'], qualifications=qualifications,
                                          max_assignments=settings['REDUNDANCY'], title=settings['TITLE'],
                                          description=settings['DESCRIPTION'], keywords=settings['KEYWORDS'],
