0c255dbe984675f4fdf10d113e0239c4d009618d
Reno Kriz <rekriz@seas.upenn.edu>
2019-02-13 14:01:53 -0500

    Debug

--- human_evaluation/quality_hit/format_input.py
+++ human_evaluation/quality_hit/format_input.py
@@ -94,6 +94,9 @@ def output_format(inputs, preds, scores, systems, output_file):
     print(len(mturk_input))
     print([len(m) for m in mturk_input])
 
+    for j in range(mturk_input[0]):
+        print(mturk_input[0][j])
+
 
 
                                 
